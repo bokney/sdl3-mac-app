1b16ddc44d8e42ed319d4a5be1197ef307637fd9
bokney <toknyk@gmail.com>
2025-02-24 21:45:23 +0000

    audio visual 🔉📺

--- src/main.c
+++ src/main.c
@@ -1,109 +1,91 @@
 
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_image.h>
-#include <stdio.h>
-#include <stdbool.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_mixer/SDL_mixer.h>
 
-#define WINDOW_WIDTH  800
-#define WINDOW_HEIGHT 600
+int main(int argc, char* argv[]) {
+    SDL_Window *window =  NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Texture *texture = NULL;
 
-int main(int argc, char *argv[]) {
-    // Initialize SDL video subsystem
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_AUDIO) < 0) {
+        SDL_Log("SDL_Init error: %s\n", SDL_GetError());
+        return -1;
     }
 
-    // Initialize SDL_image for JPEG support
-    int imgFlags = IMG_INIT_JPG;
-    if (!(IMG_Init(imgFlags) & imgFlags)) {
-        printf("IMG_Init Error: %s\n", IMG_GetError());
-        SDL_Quit();
-        return 1;
+    SDL_AudioSpec desired;
+    desired.freq = 44100;
+    desired.format = MIX_DEFAULT_FORMAT;
+    desired.channels = 2;
+    
+    if (!Mix_OpenAudio(0, &desired)) {
+        SDL_Log("SDL_mixer could not initialize! SDL_mixer Error: %s\n", SDL_GetError());
+        return -5;
     }
 
-    // Create an 800x600 window centered on the screen
-    SDL_Window *window = SDL_CreateWindow("SDL3 Mac App", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
-    if (!window) {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
+    window = SDL_CreateWindow("Minature Mouseketeer", 800, 600, 0);
+    if (window == NULL) {
+        SDL_Log("SDL_CreateWindow error: %s\n", SDL_GetError());
+        return -2;
+    }
+    
+    renderer = SDL_CreateRenderer(window, NULL);
+    if (renderer == NULL) {
+        SDL_Log("SDL_CreateRenderer error: %s\n", SDL_GetError());
+        return -3;
     }
 
-    // Create a renderer with hardware acceleration
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
+    SDL_Log("SDL3 initialised.");
 
-    // Load the JPEG image from the resources folder
-    SDL_Surface *imageSurface = IMG_Load("resources/image.jpeg");
-    if (!imageSurface) {
-        printf("IMG_Load Error: %s\n", IMG_GetError());
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-
-    // Create a texture from the loaded image
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-    SDL_FreeSurface(imageSurface);
+    texture = IMG_LoadTexture(renderer, "resources/miniature_mousketeer.jpeg");
     if (!texture) {
-        printf("SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
+        SDL_Log("IMG_LoadTexture error: %s\n", SDL_GetError());
+        return -4;
     }
 
-    // Main loop: Render the image and listen for the escape key or window close event
-    bool quit = false;
+    Mix_Music *music = Mix_LoadMUS("resources/boss_fast_version.wav");
+    if (!music) {
+        SDL_Log("SDL_mixer Error: %s\n", SDL_GetError());
+        return -6;
+    }
+    Mix_PlayMusic(music, -1);
+
+    int w = 0, h = 0;
+    SDL_FRect dst;
+    const float scale = 4.0f;
+
+    SDL_GetRenderOutputSize(renderer, &w, &h);
+    SDL_SetRenderScale(renderer, scale, scale);
+    SDL_GetTextureSize(texture, &dst.w, &dst.h);
+    dst.x = ((w / scale) - dst.w) / 2;
+    dst.y = ((h / scale) - dst.h) / 2;
+
     SDL_Event event;
+    int quit = 0;
     while (!quit) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
-            if (event.type == SDL_KEYDOWN) {
-                if (event.key.keysym.sym == SDLK_ESCAPE) {
-                    quit = true;
-                }
+            switch (event.type) {
+            case SDL_EVENT_QUIT:
+                SDL_Log("SDL3 event quit.");
+                quit = 1;
+                break;
+            default:
+                break;
             }
         }
-
-        // Clear the renderer
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0xff, 0xff);
         SDL_RenderClear(renderer);
-
-        // Query texture dimensions to center the image
-        int texW = 0, texH = 0;
-        SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
-        SDL_Rect dstRect;
-        dstRect.x = (WINDOW_WIDTH - texW) / 2;
-        dstRect.y = (WINDOW_HEIGHT - texH) / 2;
-        dstRect.w = texW;
-        dstRect.h = texH;
-
-        // Copy the texture to the renderer and present
-        SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+        SDL_RenderTexture(renderer, texture, NULL, &dst);
         SDL_RenderPresent(renderer);
-
-        // Delay to cap the frame rate (~60 FPS)
-        SDL_Delay(16);
+        SDL_Delay(1);
     }
 
-    // Clean up resources
-    SDL_DestroyTexture(texture);
+    SDL_Log("SDL3 shutdown.");
+    Mix_FreeMusic(music);
+    Mix_CloseAudio();
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    IMG_Quit();
     SDL_Quit();
 
     return 0;
